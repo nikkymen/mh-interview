2f74ba3863b5af71bb25e1843bfa10cb3a4da1a6
nikita.kozhanov <nikkymen@gmail.com>
2025-11-04 20:10:28 +0300

    Update get_features.py

--- get_features.py
+++ get_features.py
@@ -1,6 +1,8 @@
 import argparse
+import logging
 import pandas as pd
 import tempfile
+import os
 
 from pathlib import Path
 
@@ -13,15 +15,36 @@ from audio_features.opensmile_features import extract_af_opensmile
 
 # from text_features.llm_features import extract_tf_llm
 
+logging.basicConfig(format='[%(asctime)s] %(name)-15.15s [%(levelname)-8.8s]  %(message)s')
+logger = logging.getLogger('wellbeing')
+logger.setLevel('INFO')
+logger.info(f'Initialized logger {logger}')
+
 def main():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--input", type=Path)
-    parser.add_argument("--output", type=Path, default=Path("./features.parquet"))
+    parser.add_argument("--input", type=Path, required=False)
+    parser.add_argument("--output", type=Path, default=Path("./features.parquet"), required=False)
 
     args = parser.parse_args()
 
-    input_path: Path = args.input
+    env_input = os.getenv("UNIP_PIPELINE_INPUT")
+
+    input_path = args.input or (Path(env_input) if env_input else None)
+    if not input_path:
+        logger.info('Входной файл не задан.')
+        parser.error("Missing input. Provide --input or set UNIP_PIPELINE_INPUT")
+
+    if not input_path.exists():
+        logger.info(f'Файл не существует: {input_path}.')
+        return 1
+
+    env_output = os.getenv("UNIP_PIPELINE_OUTPUT")
+
+    if env_output:
+        output_path = Path(env_output) / "features.parquet"
+    else:
+        output_path = args.output
 
     #video_tracks: pd.DataFrame = extract_vf_openface(args.input)
     #vf_tsfresh: pd.DataFrame = extract_vf_tsfresh(video_tracks, 'id')
@@ -29,7 +52,11 @@ def main():
     temp_dir = tempfile.TemporaryDirectory()
     temp_path = Path(temp_dir.name)
 
+    logger.info('Извлечение аудиодорожек.')
+
     wav_raw, wav_norm = extract_wav_from_video(input_path, temp_path)
+
+    logger.info('Извлечение аудиопризнаков.')
 
     af_opensmile: pd.DataFrame = extract_af_opensmile(wav_raw)
 
@@ -49,7 +76,9 @@ def main():
 
     # Export
 
-    features.to_parquet(args.output)
+    logger.info('Экспорт.')
+
+    features.to_parquet(output_path)
 
 if __name__ == "__main__":
     main()
